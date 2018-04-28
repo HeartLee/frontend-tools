63adb31bf8a954765e596fb222265a81e417873e
JoseLee <lg282923166@gmail.com>
2018-04-28 10:45:26 +0800

    fix: css bug

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ let webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:64].js',
     path: path.resolve('dist')
   },
   module: {
@@ -29,13 +29,13 @@ module.exports = {
             vendor: {   // 抽离第三方插件
                 test: /node_modules/,   // 指定是node_modules下的第三方包
                 chunks: 'initial',
-                name: 'vendor12',  // 打包后的文件名，任意命名    
+                name: 'vendor',  // 打包后的文件名，任意命名    
                 // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
                 priority: 10    
             },
             utils: { // 抽离自己写的公共代码，utils这个名字可以随意起
                 chunks: 'initial',
-                name: 'utils34',  // 任意命名
+                name: 'utils',  // 任意命名
                 minSize: 0    // 只要超出0字节就生成一个新包
             }
         }
@@ -44,7 +44,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      chunks: ['vendor12']
+      hash: true,
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
